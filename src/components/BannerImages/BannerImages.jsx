f699c1487c0fe861471d263b9eee9cc2ae983e0a
ignaarguello <igna.arguello2110@hotmail.com>
2025-05-07 10:57:24 -0300

    Feat: banner de imagenes en home

---
+++ src/components/BannerImages/BannerImages.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+//Variables
+import Fondo from '/images/grid/Grid1.png'
+import Grid2 from '/images/manos-1.png'
+import Grid3 from '/images/grid/Grid3.png'
+import Grid4 from '/images/grid/Grid4.png'
+import Grid5 from '/images/grid/Grid5.png'
+import Grid6 from '/images/grid/Grid6.png'
+
+const ArrayImages = [Grid2, Grid3, Grid4, Grid5, Grid6, Fondo]
+
+
+export default function BannerImages() {
+    return (
+        <div id='ContenedorGeneral_BannerImages'>
+            {ArrayImages.map((e, index) => (
+                <img key={index} className='ImageGrid' src={e} alt={`Image Grid ${index + 1}`} />
+            ))}
+        </div>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,31 +4,43 @@ import React from 'react'
 import { FaWhatsapp } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 
+//Componentes
+import BannerImages from '../../components/BannerImages/BannerImages';
+
 //Estilos
 import '../../styles/estilos.css'
 
 export default function Home() {
     return (
         <div id='ContenedorPrincipal_PageHome'>
-            {/* 1 */}
-            <section className="SubContenendores">
-                <img id='LogoPrincipal_Home' src="./images/logo-circular.png" alt="" />
-                <h2 id='TituloEspacioDeAyuda'>Espacio de Ayuda</h2>
-            </section>
-            {/* 2 */}
-            <section className="SubContenendores">
-                <div id='Con_ContenidoPrincipal_Home_S2'>
-                    <h2 id='TituloQuienesSomos'>¿Quienes Somos?</h2>
-                    <p id='ParrafoAcercaDeNosotros'>Este es un lugar donde podemos charlar sobre lo que estás viviendo, desde mi propia experiencia y con la intención de que te sientas escuchado/a y comprendido/a. No soy profesional de la salud mental, pero quiero ofrecerte un espacio de contención y apoyo, como si estuviéramos tomando un café y compartiendo un momento. Si estás buscando una oreja amiga, este es tu lugar.</p>
-                    <div id='PieContenedor_Home_S2'>
-                        <h3 id='Subtitulo_LaDepresion-Yo'>©La Depresión y Yo | Espacio de ayuda emocional</h3>
+            {/* ? Vista 1 */}
+            <div id='Contenedor-Vista1_PageHome'>
+
+                {/* 1 */}
+                <section className="SubContenendores">
+                    <img id='LogoPrincipal_Home' src="./images/logo-circular.png" alt="" />
+                    <h2 id='TituloEspacioDeAyuda'>Espacio de Ayuda</h2>
+                </section>
+                {/* 2 */}
+                <section className="SubContenendores">
+                    <div id='Con_ContenidoPrincipal_Home_S2'>
+                        <h2 id='TituloQuienesSomos'>¿Quienes Somos?</h2>
+                        <p id='ParrafoAcercaDeNosotros'>Este es un lugar donde podemos charlar sobre lo que estás viviendo, desde mi propia experiencia y con la intención de que te sientas escuchado/a y comprendido/a. No soy profesional de la salud mental, pero quiero ofrecerte un espacio de contención y apoyo, como si estuviéramos tomando un café y compartiendo un momento. Si estás buscando una oreja amiga, este es tu lugar.</p>
+                        <div id='PieContenedor_Home_S2'>
+                            <h3 id='Subtitulo_LaDepresion-Yo'>©La Depresión y Yo | Espacio de ayuda emocional</h3>
+                        </div>
                     </div>
-                </div>
-                <div id='PieDePagina_Home_S2'>
-                <FaWhatsapp className='IconoRedSocial_Home' />
-                <FaInstagram className='IconoRedSocial_Home' />
-                </div>
-            </section>
+                    <div id='PieDePagina_Home_S2'>
+                        <FaWhatsapp className='IconoRedSocial_Home' />
+                        <FaInstagram className='IconoRedSocial_Home' />
+                    </div>
+                </section>
+
+            </div>
+            {/* Vista 2 */}
+            <div id='Contenedor-Vista2_PageHome'>
+                <BannerImages/>
+            </div>
         </div>
     )
 }

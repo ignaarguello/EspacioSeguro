d7560efaff56ab3a4b4c19e980d0e392c47dc5d0
ignaarguello <igna.arguello2110@hotmail.com>
2025-05-18 20:02:13 -0300

    Nuevos cambios

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,18 +18,25 @@ export default function Home() {
 
                 {/* 1 */}
                 <section className="SubContenendores">
-                    <img id='LogoPrincipal_Home' src="./images/logo-circular.png" alt="" />
-                    <h2 id='TituloEspacioDeAyuda'>Espacio de Ayuda</h2>
+                    <nav id='Navegacion_Home'>
+                        <img id='LogoPrincipal_Home' src="./images/logo-circular.png" alt="" /> 
+                        <ul id='ListaNavegacion_Home'>
+                            <span className='ItemListaNavegacion_Home'>Inicio</span>
+                            <span className='ItemListaNavegacion_Home'>Sobre mí</span>
+                            <span className='ItemListaNavegacion_Home'>Contacto</span>
+                            <span className='ItemListaNavegacion_Home'>Blog</span>
+                        </ul>
+                    </nav>
+                    <h2 id='TituloMarca_Home'>Equilibrio Mental</h2>
+                    <div id='Con_ContenidoPrincipal_Home_S2'>
+                        <p id='ParrafoAcercaDeNosotros'>Este es un lugar donde podemos charlar sobre lo que estás viviendo, desde mi propia experiencia y con la intención de que te sientas escuchado/a y comprendido/a. No soy profesional de la salud mental, pero quiero ofrecerte un espacio de contención y apoyo, como si estuviéramos tomando un café y compartiendo un momento. Si estás buscando una oreja amiga, este es tu lugar.</p>
+                        {/* <div id='PieContenedor_Home_S2'>
+                            <h3 id='Subtitulo_LaDepresion-Yo'>©Nombre Propio | Espacio de ayuda emocional</h3>
+                        </div> */}
+                    </div>
                 </section>
                 {/* 2 */}
                 <section className="SubContenendores">
-                    <div id='Con_ContenidoPrincipal_Home_S2'>
-                        <h2 id='TituloQuienesSomos'>¿Quienes Somos?</h2>
-                        <p id='ParrafoAcercaDeNosotros'>Este es un lugar donde podemos charlar sobre lo que estás viviendo, desde mi propia experiencia y con la intención de que te sientas escuchado/a y comprendido/a. No soy profesional de la salud mental, pero quiero ofrecerte un espacio de contención y apoyo, como si estuviéramos tomando un café y compartiendo un momento. Si estás buscando una oreja amiga, este es tu lugar.</p>
-                        <div id='PieContenedor_Home_S2'>
-                            <h3 id='Subtitulo_LaDepresion-Yo'>©La Depresión y Yo | Espacio de ayuda emocional</h3>
-                        </div>
-                    </div>
                     <div id='PieDePagina_Home_S2'>
                         <FaWhatsapp className='IconoRedSocial_Home' />
                         <FaInstagram className='IconoRedSocial_Home' />
@@ -40,7 +47,7 @@ export default function Home() {
             {/* Vista 2 */}
             <div id='Contenedor-Vista2_PageHome'>
                 <h3 className='TituloIntroductorio'><span className='SpanTitulo_Home'>Tu Espacio: </span>Bienvenido/a a un rincón seguro y confidencial. Aquí podés sentirte libre de explorar tus emociones, compartir tus experiencias y encontrar recursos que te acompañen en tu camino. No estás solo/a.</h3>
-                <BannerImages/>
+                {/* <BannerImages/> */}
                 <h3 className='TituloIntroductorio'><span className='SpanTitulo_Home'>Con Vos: </span>No estás en esto solo/a. Este sitio es una compañía en tu día a día, un lugar para recordarte tu fortaleza y ofrecerte apoyo cuando más lo necesitás. Juntos podemos transitar este camino</h3>
             </div>
         </div>

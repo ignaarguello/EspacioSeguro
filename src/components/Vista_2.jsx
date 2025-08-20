0410fdcbaa8f7a7d5c675207c37e63041b15d9a8
ignaarguello <igna.arguello2110@hotmail.com>
2025-08-20 19:49:22 -0300

    Add/Creacion de  vista2 - Home

--- src/components/Vista_2.jsx
+++ src/components/Vista_2.jsx
@@ -4,6 +4,13 @@ export default function Vista_2() {
     return (
         <div id='Vista-2_Home'>
             <h2 id='TituloProposito'>Nuestro Propósito</h2>
+            <div id='SubContenedorPrincipal'>
+                <div id='ContenedorTitulosPrincipal_V2'>
+                    <h3 id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
+                </div>
+                <div id='ContenedorIconos_V2'>
+                </div>
+            </div>
         </div>
     )
 }

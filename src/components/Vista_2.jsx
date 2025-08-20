b826b16478e55534f63c44fe4924e0eb728e1763
ignaarguello <igna.arguello2110@hotmail.com>
2025-08-20 20:37:03 -0300

    Add/Integracion de AOS animate

--- src/components/Vista_2.jsx
+++ src/components/Vista_2.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 
 export default function Vista_2() {
-    return (
-        <div id='Vista-2_Home'>
-            <h2 id='TituloProposito'>Nuestro Propósito</h2>
-            <div id='SubContenedorPrincipal'>
-                <div id='ContenedorTitulosPrincipal_V2'>
-                    <h3 id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
-                </div>
-                <div id='ContenedorIconos_V2'>
+    // Initialize AOS (Animate On Scroll) for animations    
+        useEffect(() => {
+            AOS.init({
+                // Optional settings object for customization
+                duration: 1000, // animation duration
+                once: true,    // whether animation should only happen once
+            });
+        }, []);
+    // Render the component
+        return (
+            <div id='Vista-2_Home'>
+                <h2 id='TituloProposito'>Nuestro Propósito</h2>
+                <div id='SubContenedorPrincipal'>
+                    <div id='ContenedorTitulosPrincipal_V2'>
+                        {/* Titulo - 1 */}
+                        <div className='ContenedorTituloIndividiaul_V2'>
+                            <h3 data-aos="fade-right" className='TituloUnLugarSeguro-1' id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
+                        </div>
+                        {/* Titulo - 2 */}
+                        <div className='ContenedorTituloIndividiaul_V2'>
+                            <h3 data-aos="fade-left" className='TituloUnLugarSeguro-2' id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
+                        </div>
+                        {/* Titulo - 3 */}
+                        <div className='ContenedorTituloIndividiaul_V2'>
+                            <h3 data-aos="fade-right" className='TituloUnLugarSeguro-3' id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
+                        </div>
+                    </div>
+                    <div id='ContenedorIconos_V2'>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }

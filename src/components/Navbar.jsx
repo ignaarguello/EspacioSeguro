e04d30360883bf591550876b8a70561651ec0201
ignaarguello <igna.arguello2110@hotmail.com>
2025-08-20 16:37:00 -0300

    Add/Mejoras de UI + responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <div id='ContenedorNavbar'>
             <img id='EspacioSeguroBrand' src="./images/logo/EspacioSeguroLogo.png" alt="" />
-            <h2 id='TituloEspacioSeguro'>Espacio Seguro</h2>
+            <h2 id='TituloEspacioSeguro'>Espacio Seguro - Sitio Web</h2>
             <div id='ContenedorLinksNavegacion'>
                 <span className='NavegacionLink'>Home</span>
                 <span className='NavegacionLink'>Proposito</span>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,6 +14,21 @@ export default function Home() {
     return (
         <div id='ContenedorPrincipal_PageHome'>
             <Navbar />
+            {/* Vista 1 */}
+            <div id='Vista-1_Home'>
+                <div id='ContenedorTitulosPrincipales'>
+                    <h2 id='TituloPazOnline'>Tu espacio de charla online</h2>
+                </div>
+                {/* Contenedor Imagen */}
+                <div className='grid-container'>
+                    <img src="./images/generales/fondo-2.png" alt="Descripción de la imagen 1" className="imagen"/>
+                    <img src="./images/generales/fondo-3.png" alt="Descripción de la imagen 1" className="imagen"/>
+                    <img src="./images/generales/terapia1.png" alt="Descripción de la imagen 1" className="imagen  imagen-completa"/>
+                </div>
+            </div>
+            {/* Vista 2 */}
+            <div id='Vista-2_Home'>
+            </div>
         </div>
     )
 }

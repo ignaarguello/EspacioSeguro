6798d563b7fe5c0f84949531964fe218f7a8f294
ignaarguello_ <arguello.ignacio_dev@hotmail.com>
2025-08-21 11:28:31 -0300

    Remove/AOS animate - momentaneamente

--- src/components/Vista_2.jsx
+++ src/components/Vista_2.jsx
@@ -5,35 +5,33 @@ import { useEffect } from 'react';
 
 export default function Vista_2() {
     // Initialize AOS (Animate On Scroll) for animations    
-        useEffect(() => {
-            AOS.init({
-                // Optional settings object for customization
-                duration: 1000, // animation duration
-                once: true,    // whether animation should only happen once
-            });
-        }, []);
+    useEffect(() => {
+        AOS.init({
+            // Optional settings object for customization
+            duration: 1000, // animation duration
+            once: true,    // whether animation should only happen once
+        });
+    }, []);
     // Render the component
-        return (
-            <div id='Vista-2_Home'>
-                <h2 id='TituloProposito'>Nuestro Propósito</h2>
-                <div id='SubContenedorPrincipal'>
-                    <div id='ContenedorTitulosPrincipal_V2'>
-                        {/* Titulo - 1 */}
-                        <div className='ContenedorTituloIndividiaul_V2'>
-                            <h3 data-aos="fade-right" className='TituloUnLugarSeguro-1' id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
-                        </div>
-                        {/* Titulo - 2 */}
-                        <div className='ContenedorTituloIndividiaul_V2'>
-                            <h3 data-aos="fade-left" className='TituloUnLugarSeguro-2' id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
-                        </div>
-                        {/* Titulo - 3 */}
-                        <div className='ContenedorTituloIndividiaul_V2'>
-                            <h3 data-aos="fade-right" className='TituloUnLugarSeguro-3' id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
-                        </div>
+    return (
+        <div id='Vista-2_Home'>
+            <h2 id='TituloProposito'>Nuestro Propósito</h2>
+            <div id='SubContenedorPrincipal'>
+                <div id='ContenedorTitulosPrincipal_V2'>
+                    {/* Titulo - 1 */}
+                    <div className='ContenedorTituloIndividiaul_V2'>
+                        <h3 className='TituloUnLugarSeguro-1' id='TituloUnLugarSeguro'>Un lugar seguro para hablar sobre la depresión. Conectá con otros y encontrá el apoyo que necesitas.</h3>
                     </div>
-                    <div id='ContenedorIconos_V2'>
+                    {/* Titulo - 2 */}
+                    <div className='ContenedorTituloIndividiaul_V2'>
+                        <h3 className='TituloUnLugarSeguro-2' id='TituloUnLugarSeguro'>La esperanza se encuentra en la conexión. Compartí tu viaje y encontrá un nuevo camino.</h3>
+                    </div>
+                    {/* Titulo - 3 */}
+                    <div className='ContenedorTituloIndividiaul_V2'>
+                        <h3 className='TituloUnLugarSeguro-3' id='TituloUnLugarSeguro'>Tu bienestar es nuestra prioridad. Un espacio sin juicios para compartir tus sentimientos.</h3>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}

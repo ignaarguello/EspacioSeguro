98a8f13f001ea30b05984ca8bdd45a10d7dfa93c
ignaarguello <igna.arguello2110@hotmail.com>
2025-08-22 10:00:13 -0300

    Update/Modificacion de estructura y separacion de componente Accordion

---
+++ src/components/AccordionFAQ.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { Accordion, Span } from "@chakra-ui/react"
+
+
+export default function AccordionFAQ() {
+    const items = [
+        { value: "a", title: "¿Cual es nuestro proposito?", text: "Some value 1..." },
+        { value: "b", title: "¿Debo Pagar?", text: "Some value 2..." },
+        { value: "c", title: "¿Como me ayudaria este espacio?", text: "Some value 3..." },
+    ]
+    return (
+        <div>
+            <Accordion.Root
+                id="AccordionRoot"
+                size='lg'
+                variant={"enclosed"}
+                collapsible
+                defaultValue={["b"]
+                }>
+                {items.map((item, index) => (
+                    <Accordion.Item id="AccordionItem" key={index} value={item.value}>
+                        <Accordion.ItemTrigger>
+                            <Span flex="1" id='AccordionItem'>{item.title}</Span>
+                            <Accordion.ItemIndicator />
+                        </Accordion.ItemTrigger>
+                        <Accordion.ItemContent>
+                            <Accordion.ItemBody id="AccordionText">{item.text}</Accordion.ItemBody>
+                        </Accordion.ItemContent>
+                    </Accordion.Item>
+                ))}
+            </Accordion.Root>
+        </div>
+    )
+}

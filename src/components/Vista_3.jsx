e43ecf79fd753622890ba9a58d537cb3ddec2896
ignaarguello <igna.arguello2110@hotmail.com>
2025-08-22 10:13:07 -0300

    Add/Textos correspondientes a cada item del accordion

--- src/components/Vista_3.jsx
+++ src/components/Vista_3.jsx
@@ -5,6 +5,7 @@ import AccordionFAQ from "./AccordionFAQ"
 export default function Vista_3() {
     return (
         <div id="Vista-3">
+            <h2 id="TituloFAQ">Preguntas Frecuentes - FAQ</h2>
             <AccordionFAQ />
         </div>
     )
